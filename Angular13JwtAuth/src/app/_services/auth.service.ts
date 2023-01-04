fe04ba4f0482d9fa476e42361465ce75fc6a1788
josepmartorell <martorelljosep>
2023-01-04 13:05:41 +0100

    LS24 api url

--- Angular13JwtAuth/src/app/_services/auth.service.ts
+++ Angular13JwtAuth/src/app/_services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const AUTH_API = 'http://jx11-env.eba-6nnp8tbt.eu-west-1.elasticbeanstalk.com/api/auth/';
+const AUTH_API = 'https://ls24-env.eba-tpjngynw.eu-west-1.elasticbeanstalk.com/api/auth/';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
--- Angular13JwtAuth/src/app/_services/user.service.ts
+++ Angular13JwtAuth/src/app/_services/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const API_URL = 'http://jx11-env.eba-6nnp8tbt.eu-west-1.elasticbeanstalk.com/api/test/';
+const API_URL = 'https://ls24-env.eba-tpjngynw.eu-west-1.elasticbeanstalk.com/api/test/';
 
 @Injectable({
   providedIn: 'root'
